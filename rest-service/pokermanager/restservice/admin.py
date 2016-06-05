280d2e4745a3e96d3bb6bc34ea765b7e81478136
Artur Staszczyk <astaszczyk@ganymede.eu>
2016-06-05 22:53:29 +0200

    Rest service added - django empty project

---
+++ rest-service/pokermanager/restservice/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import RoundModel, TournamentStructureModel
+
+admin.site.register(RoundModel)
+admin.site.register(TournamentStructureModel)
---
+++ rest-service/pokermanager/restservice/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class RoundModel (models.Model):
+
+    small_blind = models.IntegerField()
+    big_blind = models.IntegerField()
+    round_duration = models.IntegerField()
+
+    def __str__(self):
+        return str.format("Small blind: %d - Big blind: %d", self.small_blind, self.big_blind)
+
+class TournamentStructureModel(models.Model):
+
+    name = models.CharField(max_length=128)
+    rounds = models.ManyToManyField(RoundModel)
+
+    def __str__(self):
+        return self.name
