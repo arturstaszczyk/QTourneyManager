eb0d1da791f91e3d120a702c8b8febef7ccc9109
Artur Staszczyk <astaszczyk@ganymede.eu>
2016-07-22 16:59:35 +0200

    ParticipantModel added to REST service

--- rest-service/pokermanager/restservice/admin.py
+++ rest-service/pokermanager/restservice/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import RoundModel, TournamentStructureModel
+from .models import RoundModel, TournamentStructureModel, ParticipantModel, BuyinStructureModel
 
 admin.site.register(RoundModel)
 admin.site.register(TournamentStructureModel)
+admin.site.register(ParticipantModel)
+admin.site.register(BuyinStructureModel)
--- rest-service/pokermanager/restservice/models.py
+++ rest-service/pokermanager/restservice/models.py
@@ -7,7 +7,7 @@ class RoundModel (models.Model):
     round_duration = models.IntegerField()
 
     def __str__(self):
-        return str.format("Small blind: %d - Big blind: %d", self.small_blind, self.big_blind)
+        return "Small blind: {}GC - Big blind: {}GC".format(self.small_blind, self.big_blind)
 
 class TournamentStructureModel(models.Model):
 
@@ -16,3 +16,19 @@ class TournamentStructureModel(models.Model):
 
     def __str__(self):
         return self.name
+
+class BuyinStructureModel(models.Model):
+    bankroll = models.IntegerField(default=1000)
+    cash = models.IntegerField(default=5)
+
+    def __str__(self):
+        return "{}GC for {}zl".format(self.bankroll, self.cash)
+
+class ParticipantModel(models.Model):
+
+    nick = models.CharField(max_length=128, unique=True)
+    rebuy_count = models.IntegerField(default=1)
+    buyin_structure = models.ForeignKey(BuyinStructureModel, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.nick
